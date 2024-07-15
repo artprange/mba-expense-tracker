d51a7f07e1f3cba9a4135668ab593f5e0507e27e
artprange <arthur.prange@gmail.com>
2024-07-15 18:33:46 -0300

    search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Transcactions } from './pages/Transactions'
-
+import { Transactions } from './pages/Transactions/index'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transcactions />
+      <Transactions />
     </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,18 +1,21 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 import {
   PriceHighlight,
   TransactionContainer,
   TransactionTable,
 } from './styles'
 
-export function Transcactions() {
+export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
 
       <TransactionContainer>
+        <SearchForm />
+
         <TransactionTable>
           <tbody>
             <tr>
@@ -24,9 +27,9 @@ export function Transcactions() {
               <td>13/04/2022</td>
             </tr>
             <tr>
-              <td>Hamburger</td>
+              <td width="50%">Hambúrguer</td>
               <td>
-                <PriceHighlight variant="expense">-R$ 60,00</PriceHighlight>
+                <PriceHighlight variant="expense">-R$ 59,00</PriceHighlight>
               </td>
               <td>Alimentação</td>
               <td>10/04/2022</td>

2fee20bd5b92ea9e895f547ce412a7835acf3d48
artprange <arthur.prange@gmail.com>
2024-07-15 15:32:02 -0300

    table and table styling

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
 
 export function Transcactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburger</td>
+              <td>
+                <PriceHighlight variant="expense">-R$ 60,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
